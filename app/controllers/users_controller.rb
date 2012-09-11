92aeadaaa468f4e5e7af15bfb1e4823d573e5ddf
codeneko <kodeneko@gmail.com>
2012-09-12 05:50:26 +0600

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [ :index, :edit, :update, :destroy ]
+  before_filter :correct_user,   only: [ :edit, :update ]
+  before_filter :admin_user,     only: :destroy
+  before_filter :unsigned_in_user, only: [ :new, :create ]
 
   def show
     @user = User.find(params[:id])
@@ -18,4 +22,57 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    # @user = User.find(params[:id])
+  end
+
+  def update
+    # @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    unless current_user?(user)
+      user.destroy
+      flash[:success] = "User destroyed."
+    else
+      flash[:error] = "You can't destroy yourself."
+    end
+    redirect_to users_url
+  end
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+
+    def unsigned_in_user
+      redirect_to root_path, notice: "Please sign out first." if signed_in?
+    end
+
 end
